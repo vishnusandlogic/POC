980b5357472dd84578ba65d5c2ae900774dd4be4
vishnusandlogic <vishnu.b@sandlogic.com>
2024-08-09 12:24:18 +0530

    Added mention me handler

--- src/server/jeromeBot/JeromeBot.ts
+++ src/server/jeromeBot/JeromeBot.ts
@@ -1,9 +1,13 @@
 import { BotDeclaration } from "express-msteams-host";
 import * as debug from "debug";
-import { CardFactory, ConversationState, MemoryStorage, UserState, TurnContext } from "botbuilder";
+import { CardFactory, ConversationState, MemoryStorage, UserState, TurnContext, MessageFactory, StatePropertyAccessor, ActionTypes, ActivityTypes } from "botbuilder";
 import { DialogBot } from "./dialogBot";
 import { MainDialog } from "./dialogs/mainDialog";
 import WelcomeCard from "./cards/welcomeCard";
+import * as Util from 'util';
+import { Dialog, DialogSet, DialogState } from "botbuilder-dialogs";
+import { HelpDialog } from "./dialogs/helpDialog";
+// const TextEncoder = Util.TextDecoder;
 
 // Initialize debug logging module
 const log = debug("msteams");
@@ -20,8 +24,44 @@ const log = debug("msteams");
       process.env.MICROSOFT_APP_PASSWORD)
 
 export class JeromeBot extends DialogBot {
+
+
+    public readonly conversationState: ConversationState;
+    public readonly dialogs: DialogSet;
+    public dialogState: StatePropertyAccessor<DialogState>;
+    
+
     constructor(conversationState: ConversationState, userState: UserState) {
         super(conversationState, userState, new MainDialog());
+
+        this.conversationState = conversationState;
+        this.dialogState = conversationState.createProperty("dialogState");
+        this.dialogs = new DialogSet(this.dialogState);
+        this.dialogs.add(new HelpDialog());
+        this.onMessage(async (context: TurnContext): Promise<void> => {
+            switch (context.activity.type) {
+                case ActivityTypes.Message:
+                    {
+                        let text = TurnContext.removeRecipientMention(context.activity);
+                        text = text.toLowerCase();
+                        if(text.startsWith("mentionme")) {
+                            await this.handleMessageMentionMeOneToOne(context);
+                            return;
+                        }
+                        else if(text.startsWith("hello")) {
+                            await context.sendActivity(`Oh hello there buddy!`);
+                            return;
+                        }
+                        else {
+                            context.sendActivity(`Sorry, my trainer hasn't trained me for this`);
+                        }
+                    }
+                    break;
+            
+                default:
+                    break;
+            }
+        })
 
         this.onMembersAdded(async (context, next) => {
             const membersAdded = context.activity.membersAdded;
@@ -39,6 +79,20 @@ export class JeromeBot extends DialogBot {
     public async sendWelcomeCard( context: TurnContext ): Promise<void> {
         const welcomeCard = CardFactory.adaptiveCard(WelcomeCard);
         await context.sendActivity({ attachments: [welcomeCard] });
+    }
+
+    private async handleMessageMentionMeOneToOne(context: TurnContext): Promise<void> {
+
+        const mention = {
+            mentioned: context.activity.from,
+            text: `<at>${new TextEncoder().encode(context.activity.from.name)}</at>`,
+            type:'mention'
+        }
+
+        const replyActivity = MessageFactory.text(`Hi ${mention.text} from a 1:1 chat`)
+        replyActivity.entities = [mention];
+        await context.sendActivity(replyActivity);
+
     }
 
 }
